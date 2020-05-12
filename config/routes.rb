d9d47c26054d2117aa65e85bc9a61dc861998239
FALY RANAIVOSON <faly.ranaivoson@gmail.com>
2020-05-12 20:24:37 +0200

    resources static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
-  get 'static_pages/secret'
+  resources :static_pages, only: [:index, :secret]
   devise_for :users
   root 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
