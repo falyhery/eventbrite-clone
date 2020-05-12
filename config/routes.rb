7389de3509044b61893085fa54f60cdf1fe12283
FALY RANAIVOSON <faly.ranaivoson@gmail.com>
2020-05-12 20:27:35 +0200

    modification routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :static_pages, only: [:index, :secret]
+  resources :static_pages, only: [:index]
   devise_for :users
   root 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
