f7bd957381001a3e7aaee1d01ad100eb9afe2059
FALY RANAIVOSON <faly.ranaivoson@gmail.com>
2020-05-13 04:07:59 +0200

    modification root

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'static_pages/index'
   get 'static_pages/secret'
   devise_for :users
-  root 'events#index'
+  root 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
