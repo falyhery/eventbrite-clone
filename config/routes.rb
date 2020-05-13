7f6764394e31b2d7e6a74edad8c3e5c13560a5b8
FALY RANAIVOSON <faly.ranaivoson@gmail.com>
2020-05-13 03:44:35 +0200

    ajout model event

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :events
   get 'static_pages/index'
   get 'static_pages/secret'
   devise_for :users
-  root 'static_pages#index'
+  root 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
